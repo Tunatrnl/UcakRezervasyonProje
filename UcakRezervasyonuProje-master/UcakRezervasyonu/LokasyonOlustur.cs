3c325cbabc811fefb0a51777a877d64022657dce
Tuna Turanlı <tnturanli@gmail.com>
2024-01-21 15:59:52 +0300

    Ucak Rezervasyon Projesi
    Uçak Rezervasyon Proje

---
+++ UcakRezervasyonuProje-master/UcakRezervasyonu/LokasyonOlustur.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcakRezervasyonu
+{
+    internal class LokasyonOlustur : Lokasyon
+    {
+        public string Ulke { get; set; }
+        public string Sehir { get; set; }
+        public string Havalimani { get; set; }
+
+        public List<string> Lokasyonlar = new List<string>();
+
+        public LokasyonOlustur()
+        {
+            Lokasyonlar.Add("İstanbul / Türkiye, Sabiha Gökçen Havalimanı");
+            Lokasyonlar.Add("İstanbul / Türkiye, Atatürk Havalimanı");
+            Lokasyonlar.Add("Moskova / Rusya, Domodedovo Havalimanı");
+            Lokasyonlar.Add("New York City / Amerika, LuGuardia Havalimanı");
+            Lokasyonlar.Add("Muan / Kore, Muan Uluslararası Havalimanı");
+            Lokasyonlar.Add("Augsburg / Almanya, Augsburg Havalimanı");
+            Lokasyonlar.Add("Amsterdan / Hollanda, Amsterdam Schiphol Havalimanı");
+        }
+    }
+}
---
+++ UcakRezervasyonuProje-master/UcakRezervasyonu/Ucak.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UcakRezervasyonu
+{
+    internal class Ucak
+    {
+        private int UcakID { get; set; }
+        private int KoltukSayisi { get; set; }
+
+        public string Ad;
+        public char Cinsiyet;
+        public string Tarih;
+        public string Saat;
+        public string Lokasyon;
+
+        public Ucak(string AdX, char CinsiyetX)
+        {
+            Ad = AdX;
+            Cinsiyet = CinsiyetX;
+            UcakID = 1;
+            KoltukSayisi = 90;
+
+            Console.WriteLine("\nEtkin uçuşlar: ");
+            LokasyonOlustur lokasyonOlustur = new LokasyonOlustur();
+
+            for (int i = 0; i < lokasyonOlustur.Lokasyonlar.Count; i++)
+            {
+                Console.WriteLine("{0} - " + lokasyonOlustur.Lokasyonlar[i], i + 1);
+            }
+
+            bool lokasyonok = false;
+
+            while (!lokasyonok)
+            {
+                Console.Write("\nLokasyonunuzu seçiniz: ");
+                int lokasyonSecim = Convert.ToInt32(Console.ReadLine());
+
+                if (lokasyonSecim > 0 && lokasyonSecim <= lokasyonOlustur.Lokasyonlar.Count)
+                {
+                    Lokasyon = lokasyonOlustur.Lokasyonlar[lokasyonSecim - 1];
+                    lokasyonok = true;
+                }
+            }
+
+            Console.Write("\nTarih seçiniz (Gün/Ay/Yıl): ");
+            Tarih = Console.ReadLine();
+
+            Console.Write("\nSaat Seçiniz (00:00): ");
+            Saat = Console.ReadLine();
+
+            Rezervasyon rezervasyon = new Rezervasyon();
+
+            // Kullanıcı bilgilerini CSV'ye kaydet
+            SaveToCSV();
+        }
+
+        public string Tamamla()
+        {
+            string tempReturn = "";
+            Rezervasyon rezervasyon = new Rezervasyon();
+
+            if (Cinsiyet == 'E')
+                tempReturn = "\nSayın " + Ad + " bey; \nUçak numaranız: " + UcakID + "\nUçuş tarihiniz: " + Tarih + "\nUçuş Saatiniz: " + Saat + "\nLokasyonunuz: " + Lokasyon + "\nBilet ücretiniz: " + rezervasyon.Fiyat + " TL\n";
+            else if (Cinsiyet == 'K')
+                tempReturn = "\nSayın " + Ad + " hanım; \nUçak numaranız: " + UcakID + "\nUçuş tarihiniz: " + Tarih + "\nUçuş Saatiniz: " + Saat + "\nULokasyonunuz: " + Lokasyon + "\nBilet ücretiniz: " + rezervasyon.Fiyat + " TL\n";
+
+            return tempReturn;
+        }
+
+        private void SaveToCSV()
+        {
+            string csvFilePath = "musteri.csv";
+
+            try
+            {
+                // Dosya var mı kontrol et, yoksa yeni bir tane oluştur ve başlık yaz
+                if (!File.Exists(csvFilePath))
+                {
+                    using (StreamWriter sw = new StreamWriter(csvFilePath))
+                    {
+                        sw.WriteLine("Ad,Cinsiyet,UcakID,KoltukSayisi,Lokasyon,Tarih,Saat");
+                    }
+                }
+
+                // Kullanıcı verilerini CSV dosyasına ekle
+                using (StreamWriter sw = new StreamWriter(csvFilePath, true))
+                {
+                    string csvLine = $"{Ad},{Cinsiyet},{UcakID},{KoltukSayisi},{Lokasyon},{Tarih},{Saat}";
+                    sw.WriteLine(csvLine);
+                }
+
+                Console.WriteLine("Kullanıcı bilgileri musteri.csv dosyasına kaydedildi.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message);
+            }
+        }
+    }
+}
